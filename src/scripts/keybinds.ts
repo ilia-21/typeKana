ca08592e18e238fc49e4018e8d45e51dfa719202
ilia-21 <HEilia21@proton.me>
2025-07-17 23:06:31 +0300

    Clean up (?) keybinds

--- src/scripts/keybinds.ts
+++ src/scripts/keybinds.ts
@@ -21,69 +21,82 @@ export const setkeybinds = () => {
 			}
 		}
 
-		if (e.code == "Escape") {
-			// Esc when Help page is visible
-			if (!helpPageContainer.classList.contains("hidden")) helpPageContainer.classList.add("hidden");
-			if (!modsPopup.classList.contains("hidden")) modsPopup.classList.add("hidden");
+		switch (currentScreen) {
+			// ==========================================================================================================
+			case "main":
+				switch (e.code) {
+					case "Enter":
+						startTest();
+						break;
+					case "Escape":
+						if (!helpPageContainer.classList.contains("hidden")) helpPageContainer.classList.add("hidden");
+						if (!modsPopup.classList.contains("hidden")) modsPopup.classList.add("hidden");
+						break;
+					case "Space":
+						toggleAll();
+						break;
+					case "F2":
+						modsPopup.classList.toggle("hidden");
+						break;
+					// CapsLock to switch alphabets
+					case "CapsLock":
+						const keys = Object.keys(alphabets);
+						const newIndex = currentAlphabetIndex + 1 > keys.length - 1 ? 0 : currentAlphabetIndex + 1;
+						if (!capsLockLock) {
+							changeAlphabetIndex(newIndex);
+							document.querySelectorAll(".alphabet").forEach((e) => {
+								e.classList.add("hidden");
+							});
+							document.getElementById(`${keys[newIndex]}Alphabet`)!.classList.remove("hidden");
+							selectedGroups.clear();
+							switchAll("off");
+						}
+						capsLockLock = !capsLockLock;
 
-			// Esc to exit test
-			if (currentScreen == "test") {
+						document.getElementById("alphabetBtn")!.innerHTML = `${capsLockLock ? "Press it again" : "Switch <br />[CapsLock]"}`;
+						break;
+					case "Space":
+						toggleAll();
+						break;
+				}
+				// [1,2,3,4] Mods selection
+				if (!modsPopup.classList.contains("hidden") && e.code.startsWith("Digit") && document.getElementById("TTSeconds")! != document.activeElement) {
+					const modID = Number(e.code.slice(-1));
+					const toggle = (e: HTMLInputElement) => {
+						e.checked = e.checked ? false : true;
+					};
+					if (modID > 4) return;
+					toggle(document.getElementsByClassName(`Mod${modID}`)[0] as HTMLInputElement);
+					if (modID == 4) {
+						//Disable all other mods for zen
+						for (let i = 0; i <= 3; i++) {
+							toggle(document.getElementsByClassName(`Mod${i}`)[0] as HTMLInputElement);
+						}
+					}
+				}
+				if (e.shiftKey && e.code.startsWith("Digit")) {
+					// Shift+[1,2,3] Select alphabet groups
+					groupKeybinds[currentAlphabet][Number(e.code.slice(-1))]();
+				}
+
+				// Time trials
+				if (!modsPopup.classList.contains("hidden") && e.key == "`") {
+					const element = document.getElementById("TTSeconds")! as HTMLInputElement;
+					element.focus();
+					// It puts ` into the input field sometimes
+					element.value = element.value.replaceAll("`", "");
+				}
+				break;
+			// ==========================================================================================================
+			case "test":
 				// Exit to results screen if Zen is enabled
 				stats.mods.includes("ZE") ? showResultsScreen() : setScreen("main");
-				return;
-			}
-			if (currentScreen == "results") setScreen("main");
-		}
-		if ((currentScreen == "results" && e.shiftKey && e.code == "R") || (currentScreen == "main" && e.code == "Enter")) startTest();
-		if (currentScreen == "main" && e.code == "Space") toggleAll();
-		if (e.shiftKey && e.code.startsWith("Digit")) {
-			// Shift+[1,2,3] Select alphabet groups
-			groupKeybinds[currentAlphabet][Number(e.code.slice(-1))]();
-		}
-
-		// F2 mods
-		if (e.key == "F2") {
-			modsPopup.classList.toggle("hidden");
-		}
-		// [1,2,3,4] Mods selection
-		if (!modsPopup.classList.contains("hidden") && e.code.startsWith("Digit") && document.getElementById("TTSeconds")! != document.activeElement) {
-			const modID = Number(e.code.slice(-1));
-			const toggle = (e: HTMLInputElement) => {
-				e.checked = e.checked ? false : true;
-			};
-			if (modID > 4) return;
-			toggle(document.getElementsByClassName(`Mod${modID}`)[0] as HTMLInputElement);
-			if (modID == 4) {
-				//Disable all other mods for zen
-				for (let i = 0; i <= 3; i++) {
-					toggle(document.getElementsByClassName(`Mod${i}`)[0] as HTMLInputElement);
-				}
-			}
-		}
-		// Time trials
-		if (!modsPopup.classList.contains("hidden") && e.key == "`") {
-			const element = document.getElementById("TTSeconds")! as HTMLInputElement;
-			element.focus();
-			// It puts ` into the input field sometimes
-			element.value = element.value.replaceAll("`", "");
-		}
-
-		// CapsLock to switch alphabets
-		if (e.key == "CapsLock") {
-			const keys = Object.keys(alphabets);
-			const newIndex = currentAlphabetIndex + 1 > keys.length - 1 ? 0 : currentAlphabetIndex + 1;
-			if (!capsLockLock) {
-				changeAlphabetIndex(newIndex);
-				document.querySelectorAll(".alphabet").forEach((e) => {
-					e.classList.add("hidden");
-				});
-				document.getElementById(`${keys[newIndex]}Alphabet`)!.classList.remove("hidden");
-				selectedGroups.clear();
-				switchAll("off");
-			}
-			capsLockLock = !capsLockLock;
-
-			document.getElementById("alphabetBtn")!.innerHTML = `${capsLockLock ? "Press it again" : "Switch <br />[CapsLock]"}`;
+				break;
+			// ==========================================================================================================
+			case "results":
+				if (e.shiftKey && e.code == "R") startTest();
+				setScreen("main");
+				break;
 		}
 
 		if (seqIndex > 0 && e.key != sequence[seqIndex]) seqIndex = 0;
